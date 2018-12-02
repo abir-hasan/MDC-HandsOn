2e775cbd01c0d3d2c6c64622c139bf4b20594688
Abir Hasan <abirhasan89@gmail.com>
2018-12-02 21:37:56 +0600

    Android X
    adding dependencies and converting corresponding files

--- app/src/main/java/com/example/abirhasan/mdc_handson/LogInActivity.kt
+++ app/src/main/java/com/example/abirhasan/mdc_handson/LogInActivity.kt
@@ -1,7 +1,7 @@
 package com.example.abirhasan.mdc_handson
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class LogInActivity : AppCompatActivity() {
 
